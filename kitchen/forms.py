1e7b9f083dcf0a35267719620dd8e0a6632d4530
Kalipsum <onlymypost@ukr.net>
2015-02-17 18:48:36 +0200

    Revert "Small fixes"
    This reverts commit a2ac4ccadbf9d8b45829717b9d8a638ba5a5ad83.

--- kitchen/forms.py
+++ kitchen/forms.py
@@ -1,8 +1,19 @@
 from django import forms
+from kitchen.models import Ingredient
+
+ingredient = Ingredient.objects.all()
+a = []
+for item in ingredient:
+    a.append((item.pk, item.name,))
 
 
 class Filter(forms.Form):
     name = forms.CharField(min_length=3, max_length=30, required=False)
-    min = forms.FloatField(required=False, min_value=0)
-    max = forms.FloatField(required=False, min_value=0)
+    ingredients = forms.MultipleChoiceField(
+        choices=a,
+        widget=forms.CheckboxSelectMultiple,
+        required=False
+    )
+    min = forms.FloatField(required=False)
+    max = forms.FloatField(required=False)
 
